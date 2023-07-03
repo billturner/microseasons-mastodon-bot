301f1b60f38b33d12613f0e60a126af42c0a5880
Bill Turner <billturner@gmail.com>
2023-07-03 18:25:12 -0400

    Updated raw-data files
    Just for my own sanity, I've updated the DDL and the CSV files
    that contain the data for the updated sqlite3 database.

--- raw-data/schema.sql
+++ raw-data/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE "divisions" (
 	"id"	INTEGER NOT NULL UNIQUE,
 	"description"	TEXT,
-	"description_jp"	INTEGER,
-	PRIMARY KEY("id" AUTOINCREMENT)
-)
-
-CREATE TABLE "microseasons" (
-	"id"	INTEGER NOT NULL UNIQUE,
-	"division_id"	INTEGER,
-	"start_month"	INTEGER,
-	"start_day"	INTEGER,
-	"end_month"	INTEGER,
-	"end_day"	INTEGER,
 	"description_jp"	TEXT,
-	"description"	TEXT,
-	"active"	INTEGER NOT NULL DEFAULT 0 CHECK(active IN (0,1)),
-	PRIMARY KEY("id" AUTOINCREMENT)
-)
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE microseasons (
+	id INTEGER,
+	division_id INTEGER,
+	start_day INTEGER,
+	end_day INTEGER,
+	leap_start_day INTEGER,
+	leap_end_day INTEGER,
+	description TEXT,
+	description_jp TEXT,
+	active INTEGER DEFAULT (0),
+	PRIMARY KEY("id")
+);
